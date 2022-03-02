360658a7dae25839618d7aa824875cca65ba3668
matthew-pryor <pryor.matthew.a@gmail.com>
2022-03-02 16:29:03 -0600

    made the app restart if the initial name given cannot be found in the data set

--- app.js
+++ app.js
@@ -33,57 +33,37 @@ function mainMenu(person, people){
 
   /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
 
-  if(!person){
-    alert("Could not find that individual.");
-    return app(people); // restart
+  let inData;
+
+  inData = people.includes(person.person);
+
+  if(inData == false){
+      alert("Could not find that individual.");
+      return app(people); // restart
   }
 
-  else if(person.length > 1){
+  else if(inData == true){
 
-    let searchResults;
+    if(person.length > 1){
 
-    displayPeople(person)
+      let searchResults;
 
-    searchResults = searchByName(person)
+      displayPeople(person)
 
-    mainMenu(searchResults, person);
+      searchResults = searchByName(person)
 
-  }
+      mainMenu(searchResults, person);
 
-  else{
-
-    let displayOption;
-    let userSatisfaction = false
-
-    for(const el of person ){
-      displayOption = promptFor("Found " + el.firstName + " " + el.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'", autoValid)
     }
 
-    switch(displayOption){
-      case "info":
-      // TODO: get person's info
-        for(const el of person){
-          displayPerson(el)};
-      break;
+    else{
 
-      case "family":
-      // TODO: get person's family
-      break;
-      case "descendants":
-      // TODO: get person's descendants
-      break;
-      case "restart":
-      app(people); // restart
-      break;
-      case "quit":
-      return; // stop execution
-      default:
-      return mainMenu(person, people); // ask again
-    }
+      let displayOption;
+      let userSatisfaction = false
 
-    while(userSatisfaction == false){
-
-      displayOption = promptFor("Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'", autoValid)
+      for(const el of person ){
+        displayOption = promptFor("Found " + el.firstName + " " + el.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'", autoValid)
+      }
 
       switch(displayOption){
         case "info":
@@ -91,27 +71,54 @@ function mainMenu(person, people){
           for(const el of person){
             displayPerson(el)};
         break;
-    
+
         case "family":
         // TODO: get person's family
         break;
-
         case "descendants":
         // TODO: get person's descendants
         break;
-
         case "restart":
-        displayOption = true
         app(people); // restart
         break;
-
         case "quit":
-        displayOption = true
         return; // stop execution
         default:
         return mainMenu(person, people); // ask again
       }
 
+      while(userSatisfaction == false){
+
+        displayOption = promptFor("Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'", autoValid)
+
+        switch(displayOption){
+          case "info":
+          // TODO: get person's info
+            for(const el of person){
+              displayPerson(el)};
+          break;
+      
+          case "family":
+          // TODO: get person's family
+          break;
+
+          case "descendants":
+          // TODO: get person's descendants
+          break;
+
+          case "restart":
+          displayOption = true
+          app(people); // restart
+          break;
+
+          case "quit":
+          displayOption = true
+          return; // stop execution
+          default:
+          return mainMenu(person, people); // ask again
+        }
+
+      }
     }
   }
 }
